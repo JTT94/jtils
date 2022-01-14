9d78816d257e650b7d7032c9a1c51108248636d8
“JTT94” <jtthornton1994@gmail.com>
2022-01-14 12:32:14 +0000

    tidy 1 channel plot

--- jtils/plot.py
+++ jtils/plot.py
@@ -2,7 +2,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-def plot_img(img):
+def plot_img(img, **kwargs):
     img = img/ 255. if img.max() > 1. else img
     shape = img.shape
 
@@ -20,7 +20,7 @@ def plot_img(img):
         if shape[0] < shape[2]:
             img = np.moveaxis(img, 2, 0)
 
-    if len(shape) == 2:
-        plt.imshow(img, cmap='gray')
-    else:
-        plt.imshow(img)
+        if img.shape[2] == 1:
+          d = img.shape[0]
+          img = img.reshape(d,d )
+    plt.imshow(img, **kwargs)
