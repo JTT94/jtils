6b488d27b1c463ac7cec72e025391bdef612e073
“JTT94” <jtthornton1994@gmail.com>
2022-02-09 15:26:59 +0000

    add pickle_obj

--- jtils/io.py
+++ jtils/io.py
@@ -1,5 +1,32 @@
 import json
+import pickle
 
 def save_json(dictionary, fp):
-    with open(fp, "w") as outfile:
+    with open(fp, "wb") as outfile:
         json.dump(dictionary, outfile)
+
+def load_json(fp):
+    with open(fp, "rb") as infile:
+        return json.load(infile)
+
+def pickle_obj(obj, fp):
+    # open a file, where you ant to store the data
+    file = open(fp, 'wb')
+
+    # dump information to that file
+    pickle.dump(obj, file)
+
+    # close the file
+    file.close()
+
+def unpickle_obj(fp):
+    # open a file, where you ant to store the data
+    file = open(fp, 'rb')
+
+    # dump information to that file
+    obj = pickle.load(file)
+
+    # close the file
+    file.close()
+   
+    return obj
