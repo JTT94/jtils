e7a9360bac2e6a223b55e71be5edc8fae9baf442
James Thornton <jtthornton1994@gmail.com>
2022-03-06 11:14:32 +0000

    Create gif.py

---
+++ jtils/gif.py
@@ -0,0 +1,13 @@
+from PIL import Image
+import os
+
+
+def make_gif(plot_paths, output_directory='./gif', gif_name='gif'):
+    frames = [Image.open(fn) for fn in plot_paths]
+
+    frames[0].save(os.path.join(output_directory, f'{gif_name}.gif'),
+                   format='GIF',
+                   append_images=frames[1:],
+                   save_all=True,
+                   duration=100,
+                   loop=0)
